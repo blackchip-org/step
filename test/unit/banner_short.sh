5d30b622f5417d5babb672e1819205f3e8aa75db
Mike McGann <mike.mcgann@blackchip.org>
2014-05-30 17:43:02 -0400

    Refactored layout for simplicty

---
+++ test/unit/banner_short.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Copyright 2013 blackchip.org
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# TEST: Check that step banners are printed when using the -b option
+
+BASEDIR=$(dirname "$0")
+. $BASEDIR/config
+
+expected() {
+    cat <<EOF
+
+===== step4.sh: step1
+1
+
+===== step4.sh: step2
+2
+
+===== step4.sh: step3
+3
+
+===== step4.sh: step4
+4
+EOF
+}
+
+diff <($RUN -v $FX/step4.sh) <(expected) >/dev/null
+
---
+++ test/unit/error_function.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright 2013 blackchip.org
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# TEST: If there is an error in a step that is a function, the script should 
+# exit
+
+BASEDIR=$(dirname "$0")
+. $BASEDIR/config
+
+expected() {
+    cat <<EOF
+1
+EOF
+}
+
+diff <($RUN $FX/step_error_function.sh) <(expected) >/dev/null
+[ $? -eq 0 ] || exit 1
+
+# Make sure return code is passed back
+$RUN $FX/step_error_function.sh >/dev/null
+[ $? -eq 0 ] && exit 1
+exit 0
+
+
+
